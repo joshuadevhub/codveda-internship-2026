4202dedd46289814c755a9fe14a6ad2e60c701b2
joshuadevhub <elemide.j.dev@gmail.com>
2026-08-24 23:24:55 +0100

    feat: build responsive header and hero section

---
+++ Level-1/task-1-static-website/scripts/nav-toggle.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navBar = document.getElementById("primary-navigation");
+  const menuToggle = document.getElementById("menu-toggle");
+  const closeToggle = document.getElementById("close-toggle");
+  const overlay = document.querySelector(".overlay");
+
+  menuToggle.addEventListener("click", () => {
+    navBar.classList.add("isOpen");
+    menuToggle.setAttribute("aria-expanded", "true");
+    menuToggle.setAttribute("aria-label", "Close navigation menu");
+    overlay.classList.add("isOpen");
+  })
+
+  closeToggle.addEventListener("click", () => {
+    navBar.classList.remove("isOpen");
+    menuToggle.setAttribute("aria-expanded", "false");
+    menuToggle.setAttribute("aria-label", "Open navigation menu");
+    overlay.classList.remove("isOpen");
+  })
+});
