28decfd37397806d478503b4963284de3b319aee
joshuadevhub <elemide.j.dev@gmail.com>
2026-09-20 00:43:10 +0100

    feat: add student lookup by id

--- level-3/task-2-school-management/backend/server.js
+++ level-3/task-2-school-management/backend/server.js
@@ -13,10 +13,31 @@ const PORT = process.env.PORT || 5000;
 app.get("/api/students", async(req, res) => {
   try {
     const response = await pool.query('SELECT * FROM students');
-    res.status(200).send({ success: true, message: response.rows });
+    res.status(200).send({ success: true, message: "All students returned", results: response.rows });
+  } catch (err) {
+    res.status(400).send({ success: false, message: err.message });
+    return false;
+  }
+});
+
+app.get("/api/students/:id", async(req, res) => {
+  try {
+    const { id } = req.params;
+
+    const query = {
+      text: 'SELECT * FROM students WHERE id = $1',
+      values: [id]
+    }
+
+    const response = await pool.query(query);
+    if (response.rows.length === 0) {
+      res.status(404).send({ success: false, message: "Student not found" });
+      return false;
+    }
+    res.status(200).send({ success: true, message: "Student Found", result: response.rows });
   } catch (err) {
     res.status(400).send({ success: false, message: err.message });
   }
-});
+})
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
